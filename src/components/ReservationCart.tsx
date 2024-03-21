f60e048036adf6fb270ee9ed8a8ee35148a69d02
esthxrs <therjintawong@gmail.com>
2024-03-21 18:31:20 +0700

    bookSlice edited from ReservationCart

--- src/components/ReservationCart.tsx
+++ src/components/ReservationCart.tsx
@@ -2,6 +2,7 @@
 import { useAppSelector, AppDispatch } from "@/redux/store"
 import { useDispatch } from "react-redux"
 import { removeBooking } from "@/redux/features/bookSlice"
+import {bookSlice} from "@/redux/features/bookSlice"
 
 export default function BookingList() {
 
@@ -12,7 +13,7 @@ export default function BookingList() {
         <>
         <div className="text-xl px-5 py-3">Booking Informations</div>
             {bookItems.length === 0 ? (
-                <div>No Massage Shop Booking</div>
+                <div>No Massage Shop Appointment</div>
             ) : (
                 bookItems.map((bookingItem)=>(
                     <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-2" key={bookingItem.id}>
