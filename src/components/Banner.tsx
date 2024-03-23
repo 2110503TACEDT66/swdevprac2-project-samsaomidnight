22960e2727b0e114cc56b4dce0b30ce9c9a8877c
Penpitcha Ketpetch <prim.penpitcha2004@gmail.com>
2024-03-23 17:21:52 +0700

    Banner button nevigate to /massage

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -32,7 +32,7 @@ export default function Banner(){
             }
 
             <button className='bg-white text-cyan-600 border border-cyan-600 font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0 hover:bg-cyan-600 hover:text-white hover:border-transparent'
-            onClick={(e)=>{ e.stopPropagation(); router.push('/reservations') }}>
+            onClick={(e)=>{ e.stopPropagation(); router.push('/massage') }}>
                 Pick your perfect massage shop now
             </button>
         </div>
