2dfd0dce29d58186e318b91157f6507cf4f72e40
Chayanit Nualnom <punch.nualnom@gmail.com>
2024-03-25 20:33:01 +0700

    registration valid

--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -1,44 +1,50 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
 const UserSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, "Please add a name"],
-    },
-    email: {
-      type: String,
-      required: [true, "Please add an email"],
-      unique: true,
-      match: [
-        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-        "Please add a valid email",
-      ],
-    },
-    tel: {
-      type: String,
-      required: [true, "Please add a telephone number"],
-      match: [
-        /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
-        "Please add a valid telephone number",
-      ],
-    },
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
-    },
-    password: {
-      type: String,
-      required: [true, "Please add a password"],
-      minLength: 6,
-      select: false,
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-  });
-  
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+    // Add email validation regex
+  },
+  tel: {
+    type: String,
+    required: [true, "Please add a telephone number"],
+    // Add telephone number validation regex
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Hash password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
-export default User
+export default User;
