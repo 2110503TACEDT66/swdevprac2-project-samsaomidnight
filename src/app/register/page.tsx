b5b55bc7d6d89aa59c9cf887f45e5c7148874bd2
esthxrs <therjintawong@gmail.com>
2024-03-26 01:35:35 +0700

    register design

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -27,41 +27,45 @@ export default function RegisterPage() {
     }
 
     return (
-        <main className="bg-slate-100 m-5 p-5">
+        <main className="flex items-center justify-center bg-white py-20 ">
+            <div className="max-w-lg w-full space-y-8 bg-stone-400 p-10 rounded-xl shadow-2xl">
+                <h2 className="text-center text-4xl font-serif font-extrabold text-white">Register</h2>
                 <form action={addUser}>
-                <div className="text=xl text-blue-700">Register User</div>
-                <div className="flex items-center w-1/2 my-2">
-                    <label className="w-auto block text-grey-700 pr-4" htmlFor="name">Name</label>
-                    <input type="text" required id="name" name="name" placeholder="Name"
-                        className="bg-white border-2 border-grey-200 rounded w-full p-2 text-grey-700 focus:outline-none focus:border-blue-400"
-                    />
-                </div>
-                <div className="flex items-center w-1/2 my-2">
-                    <label className="w-auto block text-grey-700 pr-4" htmlFor="email">Email</label>
-                    <input type="email" required id="email" name="email" placeholder="Email"
-                        className="bg-white border-2 border-grey-200 rounded w-full p-2 text-grey-700 focus:outline-none focus:border-blue-400"
-                    />
-                </div>
-                <div className="flex items-center w-1/2 my-2">
-                    <label className="w-auto block text-grey-700 pr-4" htmlFor="tel">Phone Number</label>
-                    <input type="tel" required id="tel" name="tel" placeholder="Phone Number"
-                        className="bg-white border-2 border-grey-200 rounded w-full p-2 text-grey-700 focus:outline-none focus:border-blue-400"
-                    />
-                </div>
-                <div className="flex items-center w-1/2 my-2">
-                    <label className="w-auto block text-grey-700 pr-4" htmlFor="role">Role</label>
-                    <input type="text" required id="role" name="role" placeholder="Role"
-                        className="bg-white border-2 border-grey-200 rounded w-full p-2 text-grey-700 focus:outline-none focus:border-blue-400"
-                    />
-                </div>
-                <div className="flex items-center w-1/2 my-2">
-                    <label className="w-auto block text-grey-700 pr-4" htmlFor="password">Password</label>
-                    <input type="password" required id="password" name="password" placeholder="Password"
-                        className="bg-white border-2 border-grey-200 rounded w-full p-2 text-grey-700 focus:outline-none focus:border-blue-400"
-                    />
-                </div>
-                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded">Register User</button>
-            </form>
+                    <div className="rounded-md -space-y-px">
+                        <div className="py-3">
+                            <label htmlFor="name" className="block text-sm font-medium font-serif text-amber-50">Name</label>
+                            <input type="text" name="name" id="name" required placeholder="Your full name"
+                                   className="appearance-none relative block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" />
+                        </div>
+                        <div className="py-3">
+                            <label htmlFor="email" className="block text-sm font-medium font-serif text-amber-50">Email</label>
+                            <input type="email" name="email" id="email" required placeholder="Your email"
+                                   className="appearance-none relative block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" />
+                        </div>
+                        <div className="py-3">
+                            <label htmlFor="tel" className="block text-sm font-medium font-serif text-amber-50">Phone Number</label>
+                            <input type="tel" name="tel" id="tel" required placeholder="Your phone number"
+                                   className="appearance-none relative block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" />
+                        </div>
+                        <div className="py-3">
+                            <label htmlFor="role" className="block text-sm font-medium font-serif text-amber-50">Role</label>
+                            <input type="text" name="role" id="role" required placeholder="Your role (e.g., user, admin)"
+                                   className="appearance-none relative block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" />
+                        </div>
+                        <div className="py-3">
+                            <label htmlFor="password" className="block text-sm font-medium font-serif text-amber-50">Password</label>
+                            <input type="password" name="password" id="password" required placeholder="Create a password"
+                                   className="appearance-none relative block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" />
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit"
+                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent font-serif text-m font-large rounded-md text-gray-500 font-bold bg-amber-100 hover:bg-amber-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500">
+                            Register
+                        </button>
+                    </div>
+                </form>
+            </div>
         </main>
     );
 }
