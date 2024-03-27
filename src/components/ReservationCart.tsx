7ee1b8d0401a81165a40bf8a99da5003bf02c47f
esthxrs <therjintawong@gmail.com>
2024-03-27 15:16:57 +0700

    stuff happen

--- src/components/ReservationCart.tsx
+++ src/components/ReservationCart.tsx
@@ -18,7 +18,7 @@ export default function BookingList() {
     const fetchData = async () => {
       if (session?.user?.token) {
         const fetchedProfile = await getUserProfile(session.user.token);
-        console.log(fetchedProfile);
+        console.log("fucking profiel", fetchedProfile);
         setProfile(fetchedProfile);
       }
     };
@@ -66,12 +66,12 @@ export default function BookingList() {
           className="bg-slate-100 rounded px-5 mx-5 py-2 my-2"
           key={bookingItem.id}
         >
-          <div className="text-md font-serif">Name: {bookingItem.userName}</div>
+          <div className="text-md font-serif">Name: {profile?.data.name}</div>
           <div className="text-md font-serif">
             Massage shop: {bookingItem.massage}
           </div>
           <div className="text-md font-serif">
-            Reservation Date: {bookingItem.reserveDate}
+            Reservation Date: {bookingItem.apptDate}
           </div>
           <button
             className="m-5 text-black bg-gradient-to-r from-yellow-500 via-yellow-300 to-yellow-200 bg-opacity-90 transition-colors ease-in-out font-serif  py-2 px-8 rounded-3xl  hover:bg-gradient-to-r from-yellow-600 via-yellow-500 to-yellow-300 bg-opacity-90 transition-colors duration-300 ease-in-out"
