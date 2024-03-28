ed66851ae7848b18787d5d9cb7110556eb2e6b90
esthxrs <therjintawong@gmail.com>
2024-03-28 23:12:47 +0700

    auth seperate

---
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,44 @@
+import { AuthOptions } from "next-auth";
+import userLogIn from "@/libs/userLogIn";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+    providers: [
+      CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          email: { label: "Email", type: "email", placeholder: "email" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials, req) {
+          try {
+            if (!credentials) {
+              throw new Error("Invalid credentials");
+            }
+  
+            const user = await userLogIn(credentials.email, credentials.password);
+  
+            if (user) {
+              return user;
+            } else {
+              throw new Error("Invalid email or password");
+            }
+          } catch (error) {
+            console.error("Authentication failed:", error);
+            return null;
+          }
+        }
+      })
+    ],
+    session: { strategy: "jwt" },
+    callbacks: {
+      async jwt({ token, user }) {
+        return { ...token, ...user };
+      },
+      async session({ session, token, user }) {
+        session.user = token as any;
+        return session;
+      }
+    }
+  };
+
